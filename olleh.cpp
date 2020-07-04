1d0bfbd458f20c684e41a322f242c55d66dfffdd
LJ Chibueze <67438014+lchibueze@users.noreply.github.com>
2020-07-04 12:03:42 -0700

    making changes to olleh file

--- olleh.cpp
+++ olleh.cpp
@@ -2,9 +2,8 @@
 
 using namespace std;
 
-int main() {
-    string name;
-    getline (cin,name);
+int main(int ac, char **av) {
+    string name = av[1];
     name = string (name.rbegin (), name.rend ());
     cout <<"!" <<name << " ,olleH" << "\n";
     return 0;
