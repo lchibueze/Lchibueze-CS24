f6db53a115898d5698c6152e19f3795ed84335d3
LJ Chibueze <67438014+lchibueze@users.noreply.github.com>
2020-08-29 23:29:41 -0700

    committing files

--- vaccinate/MyScheduler.cpp
+++ vaccinate/MyScheduler.cpp
@@ -1,4 +1,7 @@
 #include "MyScheduler.h"
+#include <map>
+
+#define INFINITY INT16_MAX
 
 Scheduler* Scheduler::create(
   unsigned int                        deadline,
@@ -6,7 +9,86 @@ Scheduler* Scheduler::create(
   std::vector<std::string>            factories,
   std::vector<Route>                  routes
 ) {
-  return new MyScheduler(/* TODO */);
+  return new MyScheduler(deadline, cities, factories, routes);
 }
 
 // MyScheduler Member Functions
+
+MyScheduler::MyScheduler(unsigned int deadline, std::map<std::string, unsigned int> cities, std::vector<std::string> factories, std::vector<Route> routes) {
+  mdeadline=deadline;
+  mfactories=factories;
+  mroutes=routes;
+
+
+// bool MyScheduler::verify_route (mroutes.id) {
+  
+for (const auto &pair : cities) 
+{
+  City city;
+  city.name       = pair.first;
+  city.factory    = false;
+  city.population = pair.second;
+  city.arrivals   = new unsigned int[deadline+1]();
+  city.vaccines   = 0;
+
+  mcities[pair.first]= city;
+}
+
+for (const auto &name: factories)
+{
+  mcities[name].factory=true;
+}
+}
+
+int minimumDays (int days[], bool visited []) 
+{
+  days[]= path.days();
+  Route path;
+  int min = INFINITY, index;
+
+    for (i=0; i<mdeadline; i++)
+    {
+      if (visited[i] == false && days[i]<=min)
+      {
+        min = days[i];
+        index=i;
+      }
+    }
+  return index;
+}
+
+void MyScheduler::dijkstra(int source) {
+  Shipment Ship;
+  Route path; 
+  source = Ship.source();
+
+  source =0;
+}
+
+std::vector<Shipment> MyScheduler::schedule() {
+  //Step 1a: Set all routes to cities of infinity
+  //Step 1b: Create a map that stores the routes for all cities  
+  std::map<std::string,int> fastestRoute;
+  std::map<std::string, std::vector<int>> cityRoutes;
+  for (const auto &pair : mcities ){
+    fastestRoute[pair.first] = INFINITY;
+
+    std::vector<int> temp; //empty vector 
+    cityRoutes[pair.first] = temp;
+    
+
+    }
+
+  //Step 2: iterate through factory cities
+  for (const auto &factory : mfactories ) 
+  {
+    //Perform Dijkstra's algorithm, source node is factory
+  }
+
+
+  }
+
+}
+
+MyScheduler::~MyScheduler(){}
+
--- vaccinate/MyScheduler.h
+++ vaccinate/MyScheduler.h
@@ -4,13 +4,35 @@
 #include "Scheduler.h"
 
 class MyScheduler: public Scheduler {
+  Struct City{
+    std::string   name;
+    bool          factory;
+    unsigned int  population;
+    unsigned int  vaccines;
+    unsigned int* arrivals;
+  };
+  
   // Member Variables
+  unsigned int mdeadline; //global deadline
+  std::map<std::string, City> mcities;
+  //std::map<std::string, int> city_vaccines; // will store the number of vaccines left to deliver to a city per city
+  std::vector<std::string> mfactories;
+  std::vector<Route> mroutes;
+  //std::map<std::string, std::vector<int> > efficient_route; //fastest/cheapest route as well
+  //std::map<std::string, unsigned int> find_cheaproute; 
+
 public:
   // Constructor
+  MyScheduler(unsigned int deadline, std::map<std::string, unsigned int> cities, std::vector<std::string> factories, std::vector<Route> routes);
   // Destructor?
+  ~MyScheduler();
 
   // Required Member Function
   std::vector<Shipment> schedule();
+  //bool verify_route (/*vector<int> */mroutes.id);
+
+  int minimumDays(int days [], bool visited []);
+  void dijkstra ();
 };
 
 #endif
